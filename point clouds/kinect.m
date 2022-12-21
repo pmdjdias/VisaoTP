243a46dc9fa6c9e7263e8dbdf45d2476a285d1d3
zezocas0 <jafsantos2001@gmail.com>
2022-12-21 16:29:05 +0000

    point cloud program

---
+++ point clouds/kinect.m
@@ -0,0 +1,69 @@
+
+%% kinect a funcionar
+close all; clear all ;clear camObject;
+
+colorDevice = imaq.VideoDevice('kinect',1);
+depthDevice = imaq.VideoDevice('kinect',2);
+
+
+
+
+step(colorDevice);
+step(depthDevice);
+
+colorImage = step(colorDevice);
+
+depthImage = step(depthDevice);
+
+
+ptCloud = pcfromkinect(depthDevice,depthImage,colorImage);
+
+
+player = pcplayer(ptCloud.XLimits,ptCloud.YLimits,ptCloud.ZLimits,...
+	'VerticalAxis','y','VerticalAxisDir','down');
+
+xlabel(player.Axes,'X (m)');
+ylabel(player.Axes,'Y (m)');
+zlabel(player.Axes,'Z (m)');
+
+for i = 1:500    
+   colorImage = step(colorDevice);  
+   depthImage = step(depthDevice);
+ 
+   ptCloud = pcfromkinect(depthDevice,depthImage,colorImage);
+    
+   view(player,ptCloud);
+    time_ply=sprintf('cloud_%s.ply', datestr(now,'mm-dd-yyyy HH-MM-SS'));
+
+    Variablein = input('type 1\n( or 0 to quit)','s');
+    Variablein = str2num(Variablein);
+      if (Variablein == 1)
+      disp('gravar point cloud');
+      pcwrite(ptCloud,time_ply,"Encoding","binary")
+      
+      elseif(Variablein ==0)
+        disp("closing")
+        close all; clear all ;clear camObject;
+        return  
+      end
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
