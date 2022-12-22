c6ae733c38923451ab6789aa56d0f1587ff3384c
zezocas01 <josesantos01@ua.pt>
2022-12-22 17:29:02 +0000

    point cloud da sala

--- reconstruction_Vsize_down.py
+++ reconstruction_Vsize_down.py
@@ -10,7 +10,7 @@ def main():
     # using argparse to create UI
     parser= argparse.ArgumentParser(description='Select voxel size and a name file to resize the original point cloud ')
 
-    parser.add_argument("-v", "--voxel_size" ,type=int,default=0.05, help= "Using time mode will end the test with a duration, in seconds, equal to the value of -mv or --max_value.")
+    parser.add_argument("-v", "--voxel_size" ,type=int,default=0.012, help= "Using time mode will end the test with a duration, in seconds, equal to the value of -mv or --max_value.")
     parser.add_argument("-n ", "--name_file", type=str, help= "name of the file you")
 
     args = parser.parse_args()
@@ -26,37 +26,12 @@ def main():
 
     
     print("donwsampling point cloud ")
-    # o3d.geometry.voxel_down_sample(pcd1,args.voxel_size)    
-    pcd1.voxel_down_sample(voxel_size=args.voxel_size)
-    # convertionOfPlyToPcd("/home/jose/Documents/UA/VisaoTP/point_cloud_classroom(original)/sala119.ply","/home/jose/Documents/.pcd")
+    pcd1_down = pcd1.voxel_down_sample(voxel_size=args.voxel_size)  
 
-    newname= "filtered_"+args.name_file
-    o3d.io.write_point_cloud(newname, pcd1)
-    
+    newname= "filtered_fullroom1.ply"
+    print("saving  \n ...")
+    o3d.io.write_point_cloud(newname, pcd1_down)
 
-
-# def convertionOfPlyToPcd(ply_file,pcd_file):
-#     input_file = open(ply_file)
-#     out= pcd_file   
-#     output = open(out, 'w')
-#     write_points = False
-#     points_counter = 0
-#     nr_points = 0
-#     for s in input_file.readlines():
-#         if s.find("element vertex") != -1:
-#             nr_points = int(s.split(" ")[2].rstrip().lstrip())
-#             new_header = header.replace("XXXX", str(nr_points))
-#             output.write(new_header)
-#             output.write("\n")
-#         if s.find("end_header") != -1:
-#             write_points = True
-#             continue
-#         if write_points and points_counter < nr_points:
-#             points_counter = points_counter + 1
-#             output.write(" ".join(s.split(" ", 4)[:4]))
-#             output.write("\n")
-#     input_file.close()
-#     output.close()
 
 
 if __name__ =="__main__":
