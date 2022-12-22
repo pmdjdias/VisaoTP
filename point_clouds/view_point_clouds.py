47391751feb8872d2ee7e87d893be320da039bfe
zezocas01 <josesantos01@ua.pt>
2022-12-22 16:27:05 +0000

    point clouds renamed

--- point_clouds/view_point_clouds.py
+++ point_clouds/view_point_clouds.py
@@ -7,36 +7,31 @@ import open3d as o3d
 import argparse
 
 def main():
-    parser= argparse.ArgumentParser(description='reads point cloud from a file. ')
+    # parser= argparse.ArgumentParser(description='reads point cloud from a file. ')
 
-    parser.add_argument("-f", "--file" ,type=str, help= "add the file name.")
-    parser.add_argument("-c ", "--color", type=bool,default=False, help= "true or false, normally false, if true changes the color to orange.")
+    # parser.add_argument("-f", "--file" ,type=str, help= "add the file name.")
+    # parser.add_argument("-c ", "--color", type=bool,default=False, help= "true or false, normally false, if true changes the color to orange.")
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
+    # #color of the point cloud
+    # if args.color:
+    #     pcd1.paint_uniform_color([0, 0.6, 0.0])
     
 
     #reads the point cloud from a file
-    pcd1= o3d.io.read_point_cloud(args.file)
+    # pcd1= o3d.io.read_point_cloud(args.file)
 
-    #color of the point cloud
-    if args.color:
-        pcd1.paint_uniform_color([0, 0.6, 0.0])
-
+    pcd1=o3d.io.read_point_cloud("point_clouds/point_clouds_todas/cloud_12-21-2022 17-50-15.ply")
     
 
 
-    o3d.visualization.draw_geometries([pcd1], zoom=0.3412,
-                                    front=[0.4257, -0.2125, -0.8795],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, -0.9768, 0.2024])
+    o3d.visualization.draw_geometries([pcd1],
+			front=[ 0.23614134979016571, 0.128116079769134, -0.9632359695442585 ],
+			lookat= [ -0.45768684148788452, -0.34095668792724609, 1.6455000638961792 ],
+			up=[ -0.12323811424252881, -0.97931741385638282, -0.16046734906388865 ],
+			zoom = 0.71999999999999997)
+		
 
-
-
-
-
-
-    #the 3d view of the result
-    o3d.visualization.draw_geometries([pcd1])
 
 
 
--- reconstruction_Vsize_down.py
+++ reconstruction_Vsize_down.py
@@ -1,15 +1,16 @@
+
 import numpy as np
 import open3d as o3d
-import cv2
 import argparse
-import sys 
-import os
+
+
+
 
 def main():
     # using argparse to create UI
     parser= argparse.ArgumentParser(description='Select voxel size and a name file to resize the original point cloud ')
 
-    parser.add_argument("-v", "--voxel_size" ,type=int, help= "Using time mode will end the test with a duration, in seconds, equal to the value of -mv or --max_value.")
+    parser.add_argument("-v", "--voxel_size" ,type=int,default=0.05, help= "Using time mode will end the test with a duration, in seconds, equal to the value of -mv or --max_value.")
     parser.add_argument("-n ", "--name_file", type=str, help= "name of the file you")
 
     args = parser.parse_args()
@@ -20,40 +21,42 @@ def main():
         quit()
 
     print("Loading point cloud \n ...")
-    knot_data = o3d.data.KnotMesh()
-    mesh = o3d.io.read_triangle_mesh(knot_data.path)
     
-    pcd1= o3d.io.read_point_cloud("/home/jose/Documents/UA/VisaoTP/point_cloud_classroom(original)/sala119.ply")
+    pcd1= o3d.io.read_point_cloud(args.name_file)
 
-    convertionOfPlyToPcd("/home/jose/Documents/UA/VisaoTP/point_cloud_classroom(original)/sala119.ply","/home/jose/Documents/.pcd")
+    
+    print("donwsampling point cloud ")
+    # o3d.geometry.voxel_down_sample(pcd1,args.voxel_size)    
+    pcd1.voxel_down_sample(voxel_size=args.voxel_size)
+    # convertionOfPlyToPcd("/home/jose/Documents/UA/VisaoTP/point_cloud_classroom(original)/sala119.ply","/home/jose/Documents/.pcd")
 
-    newname= "point_cloud_filtered/point_cloud_filt-"+args.name_file + ".pcd"
+    newname= "filtered_"+args.name_file
     o3d.io.write_point_cloud(newname, pcd1)
     
 
 
-def convertionOfPlyToPcd(ply_file,pcd_file):
-    input_file = open(ply_file)
-    out= pcd_file   
-    output = open(out, 'w')
-    write_points = False
-    points_counter = 0
-    nr_points = 0
-    for s in input_file.readlines():
-        if s.find("element vertex") != -1:
-            nr_points = int(s.split(" ")[2].rstrip().lstrip())
-            new_header = header.replace("XXXX", str(nr_points))
-            output.write(new_header)
-            output.write("\n")
-        if s.find("end_header") != -1:
-            write_points = True
-            continue
-        if write_points and points_counter < nr_points:
-            points_counter = points_counter + 1
-            output.write(" ".join(s.split(" ", 4)[:4]))
-            output.write("\n")
-    input_file.close()
-    output.close()
+# def convertionOfPlyToPcd(ply_file,pcd_file):
+#     input_file = open(ply_file)
+#     out= pcd_file   
+#     output = open(out, 'w')
+#     write_points = False
+#     points_counter = 0
+#     nr_points = 0
+#     for s in input_file.readlines():
+#         if s.find("element vertex") != -1:
+#             nr_points = int(s.split(" ")[2].rstrip().lstrip())
+#             new_header = header.replace("XXXX", str(nr_points))
+#             output.write(new_header)
+#             output.write("\n")
+#         if s.find("end_header") != -1:
+#             write_points = True
+#             continue
+#         if write_points and points_counter < nr_points:
+#             points_counter = points_counter + 1
+#             output.write(" ".join(s.split(" ", 4)[:4]))
+#             output.write("\n")
+#     input_file.close()
+#     output.close()
 
 
 if __name__ =="__main__":
